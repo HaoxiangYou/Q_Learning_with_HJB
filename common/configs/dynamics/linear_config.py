a188bee40e7677baf7cc94bdcc780a8b8a1c044f
Haoxiang You <youhaox@seas.upenn.edu>
2023-05-05 21:54:22 -0400

    linear dynamics config compatible with new codes

--- common/configs/dynamics/linear_config.py
+++ common/configs/dynamics/linear_config.py
@@ -1,19 +1,16 @@
 from dataclasses import dataclass
 import gin
 import numpy as np
-from typing import Sequence 
+from typing import Sequence
+from common.configs.dynamics.dynamics_config import DynamicsConfig
 
 @gin.configurable
 @dataclass
-class LinearDynamicsConfig:
+class LinearDynamicsConfig(DynamicsConfig):
     A: Sequence[Sequence[float]]
     B: Sequence[Sequence[float]]
-    dt: float
-    umin: Sequence[float]
-    umax: Sequence[float]
 
     def __post_init__(self):
+        super().__post_init__()
         self.A = np.array(self.A)
         self.B = np.array(self.B)
-        self.umin = np.array(self.umin)
-        self.umax = np.array(self.umax)
--- common/dynamics/linear.py
+++ common/dynamics/linear.py
@@ -6,30 +6,17 @@ from typing import Tuple
 
 class LinearDynamics(Dynamics):
     def __init__(self, config: LinearDynamicsConfig) -> None:
-        super().__init__()
+        super().__init__(config)
         assert config.A.ndim == 2
         assert config.B.ndim == 2
         assert config.A.shape[0] == config.B.shape[0]
         assert config.A.shape[0] == config.A.shape[1] 
-
         self.A = config.A
         self.B = config.B
-
-        self.umin = config.umin
-        self.umax = config.umax
-
-        self.dt = config.dt
-
-    def get_dimension(self):
-        return self.A.shape[1], self.B.shape[1]
-    
-    def get_control_limit(self) -> Tuple[np.ndarray, np.ndarray]:
-        return self.umin, self.umax
 
     def states_wrap(self, x:np.ndarray) -> np.ndarray:
         return x
     
     def get_control_affine_matrix(self, x:np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-        state_dim, control_dim = self.get_dimension()
-        assert x.shape[0] == state_dim
+        assert x.shape[0] == self.state_dim
         return self.A @ x, self.B
