91f1b54fe2b606d55987a3c404d04b629e877727
Haoxiang You <haoxiang.you@yale.edu>
2023-07-25 15:21:45 -0400

    add 2D quadrotors Hovering example

--- scripts/test_vhjb_policy.py
+++ scripts/test_vhjb_policy.py
@@ -7,11 +7,13 @@ from functools import partial
 from dynamics.dynamics_basic import Dynamics
 from dynamics.linear import LinearDynamics
 from dynamics.cartpole import Cartpole
+from dynamics.quadrotors import Quadrotors2D
 from controller.controller_basic import Controller
 from controller.vhjb import VHJBController
 from controller.cartpole_energy_shaping import CartpoleEnergyShapingController
 from controller.lqr import LQR
-from configs.dynamics.dynamics_config import LinearDynamicsConfig, CartpoleDynamicsConfig
+from controller.quadrotors_model_based_controller import Quadrotors2DHoveringController
+from configs.dynamics.dynamics_config import LinearDynamicsConfig, CartpoleDynamicsConfig, Quadrotors2DConfig
 from configs.controller.vhjb_controller_config import VHJBControllerConfig
 from utils.debug_plots import visualize_loss_landscope, visualize_value_landscope_for_lqr
 
@@ -20,6 +22,8 @@ def load_systems(env_name, dynamics_config, vhjb_controller_config):
         dynamics, nn_policy, model_based_policy = load_lqr(dynamics_config, vhjb_controller_config)
     elif env_name == 'cartpole':
         dynamics, nn_policy, model_based_policy = load_cartpole(dynamics_config, vhjb_controller_config)
+    elif env_name == "quadrotors2DHovering":
+        dynamics, nn_policy, model_based_policy = load_quadrotors2D_hovering(dynamics_config, vhjb_controller_config)
 
     return dynamics, nn_policy, model_based_policy
     
@@ -91,27 +95,39 @@ def load_cartpole(dynamics_config, vhjb_controller_config):
 
     return dynamics, nn_policy, energy_shaping_controller
 
-def load_config():
-    path_to_dynamics_config_file = os.path.normpath(
+def load_quadrotors2D_hovering(dynamics_config, vhjb_controller_config):
+    if dynamics_config is None:
+        path_to_dynamics_config_file = os.path.normpath(
         os.path.join(
             os.path.dirname(__file__),
-            "../configs/dynamics/linear.gin",
+            "../configs/dynamics/quadrotors2D.gin",
+            )
         )
-    )
-    path_to_controller_config_file = os.path.normpath(
+    else:
+        path_to_dynamics_config_file = dynamics_config
+
+    if vhjb_controller_config is None:
+        path_to_vhjb_controller_config_file = os.path.normpath(
         os.path.join(
             os.path.dirname(__file__),
-            "../configs/controller/linear_vhjb_controller.gin",
+            "../configs/controller/quadrotors2DHovering_vhjb_controller.gin",
+            )
         )
-    )
+    else:
+        path_to_vhjb_controller_config_file = vhjb_controller_config
     
     gin.parse_config_file(path_to_dynamics_config_file)
-    dynamics_config = LinearDynamicsConfig()
+    dynamics_config = Quadrotors2DConfig()
 
-    gin.parse_config_file(path_to_controller_config_file)
-    controller_config = VHJBControllerConfig()
+    gin.parse_config_file(path_to_vhjb_controller_config_file)
+    vhjb_controller_config = VHJBControllerConfig()
 
-    return dynamics_config, controller_config
+    dynamics = Quadrotors2D(dynamics_config)
+    nn_policy = VHJBController(dynamics, vhjb_controller_config)
+    # initial lqr with same Q and R as neural network controller
+    quadrotor2D_hovering_controller = Quadrotors2DHoveringController(dynamics, vhjb_controller_config.xf, vhjb_controller_config.Q, vhjb_controller_config.R)
+
+    return dynamics, nn_policy, quadrotor2D_hovering_controller
 
 def test_policy(nn_policy: VHJBController, dynamics: Dynamics, model_based_controller:Controller, T=5):
     nn_policy.train_mode = False
@@ -172,7 +188,7 @@ def test_policy(nn_policy: VHJBController, dynamics: Dynamics, model_based_contr
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--env_name", default='lqr', choices=['lqr', 'cartpole'], help="Environment name")
+    parser.add_argument("--env_name", default='lqr', choices=['lqr', 'cartpole', 'quadrotors2DHovering'], help="Environment name")
     parser.add_argument("--dynamics_config", help="The path to the dynamics config")
     parser.add_argument("--vhjb_controller_config", help="The path to the config of vhjb controller")
     
@@ -187,7 +203,7 @@ def main():
     
     anim, fig = test_policy(nn_policy, dynamics, model_based_controller)
         
-    if isinstance(model_based_controller, LQR):
+    if isinstance(model_based_controller, (LQR, Quadrotors2DHoveringController)):
         P = model_based_controller.P
     elif isinstance(model_based_controller, CartpoleEnergyShapingController):
         _, P = model_based_controller.get_lqr_term()
